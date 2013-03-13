a92fcb8f05ad750ab305365b76fcc5d7b1b2ede8
Frank Cheung <fcuk112@gmail.com>
2013-03-13 12:11:31 +0000

    finish sign in.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+
+    before { visit signin_path }
+
+    it { should have_selector('h1', text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+
+    describe "after visiting another page" do
+      before { click_link "Home" }
+      it { should_not have_selector('div.alert.alert-error') }
+    end
+
+  end
+
+end
+
